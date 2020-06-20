c3ca452d29201391065ee7447adc6e6651219010
Kevin Medina <kmedinakm@hotmail.com>
2020-06-20 15:57:30 -0400

    add the client side of the app

---
+++ client/src/components/MissionKey.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const MissionKey = () => {
+  return (
+    <div>
+      <p>
+        <span className="px-3 mr-2 bg-success"> = Success</span>
+      </p>
+      <p>
+        <span className="px-3 mr-2 bg-danger"> = Fail</span>
+      </p>
+    </div>
+  );
+};
+
+export default MissionKey;
